68ccc3080ac3a585a575f0a83908fe2fab3545e5
poliyka <jkny3586@gmail.com>
2021-09-03 00:52:02 +0800

    rename OE_USER

--- odoo_install_custom.sh
+++ odoo_install_custom.sh
@@ -14,7 +14,7 @@
 # ./odoo-install
 ################################################################################
 
-OE_USER="poliyka"
+OE_USER="odoo"
 OE_FOLDER="odoo-project"
 OE_HOME="/home/$OE_USER/$OE_FOLDER"
 OE_HOME_EXT="/home/$OE_USER/$OE_FOLDER/odoo-server"
